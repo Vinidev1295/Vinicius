5acefa4e4078a7fb4f60ba4c06b44e51557e81a3
Vinidev1295 <vinidias1295@gmail.com>
2024-09-13 11:19:14 -0300

    att

---
+++ index.js
@@ -0,0 +1,9 @@
+const nome = "Vinicius"
+//nao muda, uma constante//
+let sobrenome = "Dias,"
+//pode mudar//
+var idade = 28
+//uma constante e pode mudar//
+let estudante= true
+console.log("Olá!", "Meu nome é ", nome, sobrenome, " Minha idade é: " + idade + ". Sou um estudante?", estudante)
+//testando//
