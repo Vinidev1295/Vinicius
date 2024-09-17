56ff7b7f96eeab136989922f605ab56d18e8d8d3
Vinidev1295 <vinidias1295@gmail.com>
2024-09-17 11:50:25 -0300

    Delete comparador igual.js

--- comparador igual.js
+++
@@ -1,9 +0,0 @@
-"1" === "2" // false, são diferentes
-"2" === "2" // true, são iguais
-2 === "2"  // false, tipo diferente
-
-const condicao = 1 === 2
-console.log(condicao)
-// o valor que sai da comparação 
-// pode ser guardado em uma variável
-// nesse caso, condicao === false
