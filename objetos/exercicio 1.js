665a703f64fcc35edb53e2708d8722d6087f18c7
Vinidev1295 <vinidias1295@gmail.com>
2024-09-25 10:41:47 -0300

    Create exercicio 1.js

---
+++ objetos/exercicio 1.js
@@ -0,0 +1,10 @@
+const filme = {
+    diretor:"George Lucas",
+    nome:"star wars",
+    ano:"1977",
+    elenco:["Mark Hamill, Harrison Ford, Carrie Fisher"]
+}
+console.log(filme.diretor)
+console.log(filme.nome)
+console.log(filme["ano"])
+console.log(filme["elenco"])
