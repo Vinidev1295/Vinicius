934dbad2f9a04d0e016297c328ade0c85ffae3bc
Vinidev1295 <vinidias1295@gmail.com>
2024-09-25 11:40:37 -0300

    Create exercicio 4.js

---
+++ objetos/exercicio 4.js
@@ -0,0 +1,18 @@
+const pessoa = {
+    nome:"Vinicius",
+    idade:28,
+    generoMusical: "Hip Hop"
+}
+function pessoas2(comidas){
+    infopessoa = pessoa
+}
+const pessoa2 = {
+    ...pessoa,
+    comidas:["lasanha", "batata frita"],
+    amigo:{
+        nome: "João",
+        idade: 59,
+    }
+    
+}
+console.log("O nome da pessoa é", pessoa2.nome, "e suas comidas preferidas são" ,pessoa2.comidas[0], "e" ,pessoa2.comidas[1], ".", "Seu melhor amigo se chama", pessoa2.amigo.nome,  "e tem", pessoa2.amigo.idade, "anos.")
