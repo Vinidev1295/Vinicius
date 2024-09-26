8babfeed3ae1952e02c30617954778f3fdd9f1ad
Vinidev1295 <vinidias1295@gmail.com>
2024-09-26 10:00:59 -0300

    Update exercicio 1.js

--- objetos/exercicio 1.js
+++ objetos/exercicio 1.js
@@ -2,9 +2,11 @@ const filme = {
     diretor:"George Lucas",
     nome:"star wars",
     ano:"1977",
-    elenco:["Mark Hamill, Harrison Ford, Carrie Fisher"]
+    elenco:["Mark Hamill, Harrison Ford, Carrie Fisher"],
+    visto : true
 }
 console.log(filme.diretor)
 console.log(filme.nome)
 console.log(filme["ano"])
 console.log(filme["elenco"])
+console.log(filme["visto"])
