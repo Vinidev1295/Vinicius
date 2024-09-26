6cabe6b2e95138140e3faf9b6cd9691d2624aa6d
Vinidev1295 <vinidias1295@gmail.com>
2024-09-26 11:47:38 -0300

    Create exercicio 1.js

---
+++ condicionais/exercicio 1.js
@@ -0,0 +1,8 @@
+function condicao(num1,num2){
+    if(num1===num2){
+        console.log("sucesso")
+}
+}
+const num1 = Number(prompt("Diga um número:"))
+const num2 = Number(prompt("Diga um número:"))
+condicao(num1, num2)
