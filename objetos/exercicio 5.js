bc601c8f8a74d8f154172cfb23cf1a388c0ce182
Vinidev1295 <vinidias1295@gmail.com>
2024-09-26 09:50:32 -0300

    Create exercicio 5.js

---
+++ objetos/exercicio 5.js
@@ -0,0 +1,8 @@
+const exercicio = {
+    nome: "Vinicius",
+    apelidos: ["Vini", "Vinao", "Vininho"]
+}
+function atividade(){
+console.log("Eu sou", exercicio.nome, "mas pode me chamar de:", exercicio.apelidos[0],",", exercicio.apelidos[1], "ou", exercicio.apelidos[2], ".")
+}
+atividade()
