2c67dfc9f56c7b4a23548d7ccc80d9a1b2199fc1
Vinidev1295 <vinidias1295@gmail.com>
2024-09-27 09:45:10 -0300

    Create exercicio 2.js

---
+++ condicionais/exercicio 2.js
@@ -0,0 +1,10 @@
+function condicao(num1,num2){
+if(num1===num2){
+        console.log("Os números são iguais.")
+} else {
+        console.log("Os números são diferentes.")
+}
+}
+const num1 = Number(prompt("Diga um número:"))
+const num2 = Number(prompt("Diga um número:"))
+condicao(num1, num2)
