7b768d0712168c8175361797b79a1402780594d6
Vinidev1295 <vinidias1295@gmail.com>
2024-09-27 11:15:27 -0300

    Update exercicio 2.js

--- objetos/exercicio 2.js
+++ objetos/exercicio 2.js
@@ -12,4 +12,4 @@ console.log(cachorro) //Irá imprimir o objeto cachorro
 console.log(gato) //Irá espelhar o objeto cachorro e substituir o nome por Juba
 console.log(tartaruga) //Irá espelhar o objeto gato e substituir todas letras "a" do nome pela letra "o"
 /*b) O que faz a sintaxe dos três pontos antes do nome de um objeto?
-  Vai espelhar o objeto citado.
+  Vai espelhar o objeto citado.*/
