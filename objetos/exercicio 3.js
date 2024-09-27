36bc4f7e55bbabd15f48ad137d0852a0e213de86
Vinidev1295 <vinidias1295@gmail.com>
2024-09-27 10:37:42 -0300

    Update exercicio 3.js

--- objetos/exercicio 3.js
+++ objetos/exercicio 3.js
@@ -8,5 +8,11 @@ const pessoa = {
   backender: false
 }
 
-console.log(minhaFuncao(pessoa, "backender")) //Irá imprimir o resultado da propriedade backender do objeto pessoa.
-console.log(minhaFuncao(pessoa, "altura")) // Irá imprimir como undefined pois nao definido nenhuma propriedade para altura.
+console.log(minhaFuncao(pessoa, "backender"))
+console.log(minhaFuncao(pessoa, "altura")) 
+/*a) O que vai ser impresso no console?
+Irá imprimir o resultado da propriedade backender do objeto pessoa.
+Irá imprimir como undefined.
+b) Explique o valor impresso no console. Você sabe por que isso aconteceu?
+Irá imprimir o resultado da propriedade backender do objeto pessoa.
+Irá imprimir como undefined pois nao foi definido nenhuma propriedade para altura.
