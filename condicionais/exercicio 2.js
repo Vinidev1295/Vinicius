3f4a07f7900f60691579c33b6424d543353cbd82
Vinidev1295 <vinidias1295@gmail.com>
2024-09-27 10:12:46 -0300

    Update exercicio 2.js

--- condicionais/exercicio 2.js
+++ condicionais/exercicio 2.js
@@ -1,10 +1,13 @@
 function condicao(num1,num2){
-if(num1===num2){
-        console.log("Os números são iguais.")
+let condicao2= (num1 === num2)
+if(condicao2){
+    return "Os números são iguais."
+        
 } else {
-        console.log("Os números são diferentes.")
+    return "Os numeros são diferentes"
 }
 }
 const num1 = Number(prompt("Diga um número:"))
 const num2 = Number(prompt("Diga um número:"))
-condicao(num1, num2)
+const resultado = condicao(num1,num2)
+console.log(resultado)
