463631a0ad7f11d0662cd1dcfff475258f2dae03
Vinidev1295 <vinidias1295@gmail.com>
2024-09-30 10:16:03 -0300

    Create exercicio 4.js

---
+++ condicionais/exercicio 4.js
@@ -0,0 +1,15 @@
+let pokemon = prompt("Diga o seu pokemon: ")
+switch(pokemon){
+  case 'Bulbasauro':
+  console.log("Planta e Veneno")
+  break
+  case 'Charmander':
+  console.log("Fogo")
+  break   
+  case 'Squirtle':
+  console.log("Água")
+  break
+  default:
+    console.log("O pokemon selecionado não existe.")
+  break
+}
