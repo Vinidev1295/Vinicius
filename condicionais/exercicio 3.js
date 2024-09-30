74b70ab036cce59ba89b897be58010b8dd61d750
Vinidev1295 <vinidias1295@gmail.com>
2024-09-30 10:40:53 -0300

    Update exercicio 3.js

--- condicionais/exercicio 3.js
+++ condicionais/exercicio 3.js
@@ -1,1 +1,12 @@
-
+function condicao(num1,num2){
+    if(num1>num2){
+        console.log("O número é maior ")
+    }else if(num1<num2){
+      console.log("O número é menor ")
+    }else{
+      console.log("Os números são iguais ")
+}
+}
+const num1 = Number(prompt("Diga um número:"))
+const num2 = Number(prompt("Diga um número:"))
+condicao(num1, num2)
