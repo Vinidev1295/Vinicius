6f6391b27b4a56a2cc11de53cab021da94f77461
Vinidev1295 <vinidias1295@gmail.com>
2024-09-30 10:52:20 -0300

    Update exercicio 5.js

--- condicionais/exercicio 5.js
+++ condicionais/exercicio 5.js
@@ -1,8 +1,11 @@
 let concluido = prompt("Você concluiu o ensino médio? ")
 let idade = prompt("Qual sua idade? ")
 let faculdade = prompt("Você está cursando outra faculdade? ")
+function estudar(concluido,idade,faculdade){
 if(concluido==="sim"&&idade>=18&&faculdade==="nao"){
   console.log("Você pode estudar!")
 }else{
   console.log("Você não pode estudar na faculdade ainda!")
 }
+}
+estudar(concluido,idade,faculdade)
