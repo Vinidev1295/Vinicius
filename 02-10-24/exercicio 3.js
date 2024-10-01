b029e8607db7b64a823807c3407e4f6dd384365b
Vinidev1295 <vinidias1295@gmail.com>
2024-10-01 11:14:01 -0300

    Create exercicio 3.js

---
+++ 02-10-24/exercicio 3.js
@@ -0,0 +1,7 @@
+const array = ["Oi", "sumido", "tudo", "bem?", "Saudades"]
+function teste (array){
+for(let recfrase of array){
+    console.log(`${array[0]} ${array[1]} ${array[2]} ${array[3]} ${array[4]}`)
+}
+}
+teste(array)
